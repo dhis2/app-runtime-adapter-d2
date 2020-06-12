a52edcf167370c8410e0a1f57c2762aede599d89
Jen Jones Arnesen <jennifer@dhis2.org>
2020-06-12 08:53:55 -0700

    fix: undo some prettier

--- src/D2Shim.js
+++ src/D2Shim.js
@@ -6,10 +6,10 @@ export const D2Shim = ({ appConfig, onInitialized, children }) => {
     const { d2, d2Error } = useD2({ appConfig, onInitialized })
 
     return children({ d2, d2Error })
-};
+}
 
 D2Shim.propTypes = {
     onInitialized: PropTypes.func,
     appConfig: PropTypes.object,
-    children: PropTypes.func.isRequired,
+    children: PropTypes.func.isRequired
 }
