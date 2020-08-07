0f1e551a20c6427737584e9502af79023e7f49bb
Jen Jones Arnesen <jennifer@dhis2.org>
2020-08-07 11:57:59 +0200

    chore: add unit tests (#8)

---
+++ src/__tests__/D2Shim.spec.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { D2Shim } from '../D2Shim'
+import { render } from '@testing-library/react'
+import * as useD2 from '../useD2'
+import '@testing-library/jest-dom'
+
+// this polyfill shouldn't be needed once Jest is upgraded to 26
+window.MutationObserver = require('mutation-observer')
+
+jest.mock('../useD2')
+
+describe('D2Shim', () => {
+    beforeEach(() => {
+        jest.restoreAllMocks()
+    })
+    it('returns a component with children passed in', () => {
+        useD2.useD2 = jest
+            .fn()
+            .mockReturnValue({ d2: null, d2Error: undefined })
+
+        const { container } = render(
+            <D2Shim>
+                {({ d2 }) => {
+                    if (!d2) {
+                        return null
+                    }
+                    return <div d2={d2} />
+                }}
+            </D2Shim>
+        )
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+    it('returns a component with children passed in', () => {
+        useD2.useD2 = jest
+            .fn()
+            .mockReturnValue({ d2: 'd2obj', d2Error: undefined })
+
+        const { container } = render(
+            <D2Shim>
+                {({ d2 }) => {
+                    if (!d2) {
+                        return null
+                    }
+                    return <div d2={d2} />
+                }}
+            </D2Shim>
+        )
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+})
